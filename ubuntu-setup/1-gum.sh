940b3995032f5ae47c1b85e3f83b490e37b0d349
Bitteneite <5614646+Bitteneite@users.noreply.github.com>
2024-02-10 12:26:21 +0400

    Update 1-gum.sh

--- ubuntu-setup/1-gum.sh
+++ ubuntu-setup/1-gum.sh
@@ -8,3 +8,7 @@ sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://repo.charm.sh/apt/gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/charm.gpg
 echo "deb [signed-by=/etc/apt/keyrings/charm.gpg] https://repo.charm.sh/apt/ * *" | sudo tee /etc/apt/sources.list.d/charm.list
 sudo apt update && sudo apt install gum
+
+sh ./2-install.sh
+sh ./3-setup.sh
+cd ~/dotscripts/install-scripts
