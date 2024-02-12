a679109839a53ce435b44b7f29a328da264a3fca
Bitteneite <5614646+Bitteneite@users.noreply.github.com>
2024-02-12 12:11:09 +0400

    Update install-vim.sh

--- install-scripts/install-vim.sh
+++ install-scripts/install-vim.sh
@@ -3,6 +3,8 @@
 install() {
 	sudo nala install -y vim
 	stow vim -d ~/dotfiles
+	curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 }
 
 install
